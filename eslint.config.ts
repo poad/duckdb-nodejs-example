1083db56696dd26ca94c5535fd003c8bdd5b1bc8
Kenji Saito <ken-yo@mbr.nifty.com>
2026-02-12 22:11:26 +0900

    Bumps node modules

---
+++ eslint.config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+ 
+import { parser, configs } from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+// @ts-expect-error ignore type error
+import pluginPromise from 'eslint-plugin-promise';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: [
+      '**/*.d.ts',
+      'node_modules/**/*',
+      'out',
+      'cdk.out',
+      'dist',
+    ],
+  },
+  eslint.configs.recommended,
+  ...configs.strict,
+  ...configs.stylistic,
+  pluginPromise.configs['flat/recommended'],
+  {
+    files: ['**/*.ts'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        // You will also need to install and configure the TypeScript resolver
+        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+        'typescript': true,
+        'node': true,
+      },
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
+    },
+  },
+);
