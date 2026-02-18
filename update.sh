62b0a6557a5b6c97f21a2d3dd41b5eb9b20219ac
Kenji Saito <ken-yo@mbr.nifty.com>
2026-02-18 21:00:15 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-npx -y pnpm@latest self-update && pnpm install && pnpm up -r && pnpm audit --fix && pnpm up -r  && pnpm -r lint-fix && pnpm -r build && pnpm install
+npx -y pnpm@latest self-update && pnpm install && pnpm up -r && pnpm audit --fix && pnpm up -r  && pnpm -r lint-fix && pnpm -r build && pnpm install -r --no-frozen-lockfile
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
